f3028c52a10c4871c58174dc507e8ed13e768351
O-Semenov <world.shit@yandex.ru>
2023-03-05 17:19:45 +0300

    SCAN: Fix errors in templates, tried draw my data on chart

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-
 from core.settings import BASE_DIR, READY_FILES_ROOT
 from .forms import ScanForm
 from django.shortcuts import render
@@ -16,19 +15,20 @@ import os
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {'segment': 'index'}
-
-    html_template = loader.get_template('home/index.html')
-    return HttpResponse(html_template.render(context, request))
+    labels = ['sdac', 'asdcas', 'sdacasdc']
+    values = [1, 2, 3]
+    context = {
+        'segment': 'index',
+        'labels': labels,
+        'values': values
+    }
+    return render(request, 'home/index.html', context)
 
 
 @login_required(login_url="/login/")
 def pages(request):
     context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
     try:
-
         load_template = request.path.split('/')[-1]
 
         if load_template == 'admin':
